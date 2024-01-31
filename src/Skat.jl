c64a7d2c4a7ef1348c10a119889ca557938ba06e
Sven Duve <svenduve@gmail.com>
2024-01-31 12:46:12 +0100

    shuffle and deal

--- src/Skat.jl
+++ src/Skat.jl
@@ -1,5 +1,41 @@
 module Skat
 
+using Random
 # Write your package code here.
 
+export Card, Deck, create_deck, shuffle!, deal!, start_game, PLAYERDEAL, SKATDEAL
+
+
+PLAYERDEAL = [3, 4, 3]
+SKATDEAL = [2, 0, 0]
+
+
+struct Card
+    suit::String
+    rank::String
 end
+
+struct Deck
+    cards::Array{Card}
+end
+
+# Code for initializing and managing a game of Skat
+function create_deck()
+    suits = ["Kreuz", "Pik", "Herz", "Karo"]
+    ranks = ["7", "8", "9", "Bube", "Dame", "König", "10", "Ass"]
+    return Deck([Card(suit, rank) for suit in suits for rank in ranks])
+end
+
+function shuffle!(deck::Deck)
+    Random.shuffle!(deck.cards)
+end
+
+
+function deal!(deck::Deck, n::Int)
+    return [pop!(deck.cards) for _ in 1:n]
+end
+
+
+
+end
+
